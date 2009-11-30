bdba485b36b962938a05c6dc65538a88634ab09c
dwdyer <dan@uncommons.org>
2009-11-30 18:40:07 +0000

    Initial files.

---
+++ watchmaker/index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>The Watchmaker Framework for Evolutionary Computation (evolutionary/genetic algorithms/programming for Java)</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name="description" content="Watchmaker is an extensible, high-performance, multi-threaded, object-oriented framework for implementing platform-independent evolutionary computation (evolutionary/genetic algorithms/programming) in Java." />
+    <meta name="keywords" content="evolution, evolutionary, genetic, computation, algorithms, programming, java, open, source, free, watchmaker, framework, dan, daniel, dyer, swing, sudoku, mona, lisa, biomorphs, travelling, traveling, salesman" />
+    <meta name="author" content="Daniel Dyer" />
+    <link href="watchmaker.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
+      <div id="title">
+        <h1>The Watchmaker Framework for Evolutionary Computation</h1>
+        <div id="tagline">An Open Source evolutionary/genetic algorithms toolkit for Java</div>
+      </div>
+    </div>
+  </body>
+</html>
