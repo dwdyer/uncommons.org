4369b38124279e77f19b4cd82fc04825dc558cdd
dwdyer <dan@uncommons.org>
2010-12-02 19:07:51 +0000

    Added Zeitgeist page.

---
+++ zeitgeist/index.php
@@ -0,0 +1,37 @@
+<?php echo '<?xml version="1.0" encoding="utf-8" ?>'."\n" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <title>Zeitgeist - Intelligent RSS News Aggregator</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name="description" content="Zeitgeist is an intelligent RSS news aggregator." />
+    <meta name="keywords" content="zeitgeist, rss, news, aggregator, feed, java, html, open, source, free, dan, daniel, dyer, uncommons" />
+    <meta name="author" content="Daniel Dyer" />
+    <link href="zeitgeist.css" rel="stylesheet" type="text/css" />
+    <?php readfile('analytics.html'); ?>
+  </head>
+  <body>
+    <div id="container">
+      <?php readfile('header.html'); ?>
+      <p>
+        Zeitgeist is a Java library for identifying common topics among a set of news articles downloaded from RSS feeds.
+        It groups and ranks related articles.  It is loosely based on the non-negative matrix factorisation example presented
+        in chapter 10 of Toby Segaran's Programming Collective Intelligence book.
+      </p>
+      <img src="sample.png" width="447" height="336" alt="Sample HTML output." style="border: 1px dashed #888888; float: right; margin-left: 1em; margin-bottom: 1em;" />
+      <h3>Sample HTML Output</h3>
+      <p>
+        Zeitgeist includes a basic HTML publisher for generating a page of news
+        headlines, complete with relevant images extracted from the feed articles.
+        Refer to the <a href="http://github.com/dwdyer/zeitgeist/blob/master/README.txt">README file</a> for instructions on
+        how to use Zeitgeist to generate a web page of news topics.  The default output looks something like the image on the right
+        (it can be customised with CSS).
+      </p>
+      <h3>Zeitgeist in Action</h3>
+      <p>
+        Zeitgeist is used to generate the football news headlines at <a href="http://news.footballpredictions.net">news.footballpredictions.net</a>.
+      </p>
+      <?php readfile('footer.html'); ?>
+    </div>
+  </body>
+</html>
