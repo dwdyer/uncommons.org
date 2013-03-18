661a1a1d399ad780176efb1fb3655ea8036b6c04
dwdyer <dan@uncommons.org>
2013-03-18 22:14:09 +0000

    Added missing RSS/Atom link tags.

--- newadventuresinsoftware/wp-content/themes/uncommons/header.php
+++ newadventuresinsoftware/wp-content/themes/uncommons/header.php
@@ -6,6 +6,8 @@
     <meta name="keywords" content="software, engineering, development, programming, java, haskell, python, php, objective-c, android, ios, open, source, mobile, evoultionary, computation, genetic, algorithms, artificial, intelligence"/>
     <meta name="description" content="Software development blog written by Dan Dyer." />
     <meta name="author" content="Dan Dyer" />
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="http://blog.uncommons.org/feed/" />
+    <link rel="alternate" type="application/atom+xml" title="Atom" href="http://blog.uncommons.org/feed/atom/" />
     <link href="http://uncommons.org/uncommons.css" rel="stylesheet" type="text/css" />
     <style type="text/css" media="screen">
       @import url( <?php bloginfo('stylesheet_url'); ?> );
