2c4323f69b90a8b80b9864769b09cd699577f8e6
dwdyer <dan@uncommons.org>
2013-09-03 01:01:28 +0100

    Simplified JS.

--- website/amazon.js
+++ website/amazon.js
@@ -24,25 +24,18 @@ function initAmazonLinks()
             links[i].href = links[i].href.replace(ukAddress, usAddress);
             links[i].href = links[i].href.replace(ukId, usId);
         }
-    };
+    }
 }
 
-// http://onlinetools.org/articles/unobtrusivejavascript/chapter4.html
 function addEvent(obj, evType, fn)
 { 
     if (obj.addEventListener)
     { 
         obj.addEventListener(evType, fn, false); 
-        return true; 
     }
     else if (obj.attachEvent)
     { 
-        var r = obj.attachEvent("on"+evType, fn); 
-        return r; 
+        obj.attachEvent("on"+evType, fn); 
     }
-    else
-    { 
-        return false; 
-    } 
 }
 
