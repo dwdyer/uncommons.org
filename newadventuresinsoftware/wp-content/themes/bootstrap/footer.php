d190a0ee4a64b6e9ea4056c240a789c5ed1facfe
dan <dan@uncommons.org>
2016-09-12 16:23:36 +0100

    Added Piwik analytics to blog.

--- newadventuresinsoftware/wp-content/themes/bootstrap/footer.php
+++ newadventuresinsoftware/wp-content/themes/bootstrap/footer.php
@@ -2,7 +2,7 @@
     </div>
     <div id="footer" class="text-center">
       <small class="text-muted">
-        &copy; 2006-2015 <a href="http://dandyer.co.uk">Daniel W. Dyer</a>
+        &copy; 2006-2016 <a href="http://dandyer.co.uk">Daniel W. Dyer</a>
       </small>
       <?php wp_footer();?>
     </div>
--- newadventuresinsoftware/wp-content/themes/bootstrap/header.php
+++ newadventuresinsoftware/wp-content/themes/bootstrap/header.php
@@ -6,18 +6,33 @@
     <meta name="keywords" content="software, engineering, development, programming, java, haskell, python, php, objective-c, android, ios, open, source, mobile, evoultionary, computation, genetic, algorithms, artificial, intelligence"/>
     <meta name="description" content="Software development blog written by Dan Dyer." />
     <meta name="author" content="Dan Dyer" />
-    <link rel="alternate" type="application/rss+xml" title="RSS" href="http://blog.uncommons.org/feed/" />
-    <link rel="alternate" type="application/atom+xml" title="Atom" href="http://blog.uncommons.org/feed/atom/" />
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="http://blog.dandyer.co.uk/feed/" />
+    <link rel="alternate" type="application/atom+xml" title="Atom" href="http://blog.dandyer.co.uk/feed/atom/" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="http://netdna.bootstrapcdn.com/bootswatch/3.0.0/flatly/bootstrap.min.css" rel="stylesheet"/>
+    <link href="//netdna.bootstrapcdn.com/bootswatch/3.0.0/flatly/bootstrap.min.css" rel="stylesheet"/>
     <style type="text/css" media="screen">
       @import url( <?php bloginfo('stylesheet_url'); ?> );
     </style>
     <?php wp_get_archives('type=monthly&format=link'); ?>
     <?php wp_head(); ?>
-    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="http://uncommons.org/amazon.js"></script>
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      _paq.push(["setCookieDomain", "*.dandyer.co.uk"]);
+      _paq.push(["setDomains", ["*.dandyer.co.uk","*.blog.dandyer.co.uk"]]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="https://rectangular.it/analytics/js/";
+        _paq.push(['setTrackerUrl', u]);
+        _paq.push(['setSiteId', '3']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="https://rectangular.it/analytics/js/?idsite=3" style="border:0;" alt="" /></p></noscript>
   </head>
   <body>
     <div id="wrap">
