f19dc64cc509b0d91ee4e5082de158c6e5836b3c
Daniel Dyer <dan@uncommons.org>
2019-02-04 15:37:54 +0000

    Updated blog copyright year.

--- newadventuresinsoftware/wp-content/themes/bootstrap/footer.php
+++ newadventuresinsoftware/wp-content/themes/bootstrap/footer.php
@@ -2,7 +2,7 @@
     </div>
     <div id="footer" class="text-center">
       <small class="text-muted">
-        &copy; 2006-2017 <a href="https://dandyer.co.uk">Daniel W. Dyer</a>
+        &copy; 2006-2019 <a href="https://dandyer.co.uk">Daniel W. Dyer</a>
       </small>
       <?php wp_footer();?>
     </div>
